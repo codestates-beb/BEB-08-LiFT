d23d7cd21653a64c3b14e741125bb920e2b91741
YouJun-IWON <tndhworl1998@gmail.com>
2023-04-04 17:32:03 +0900

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head />;
       <body>
-        <Main />
-        <NextScript />
+        ;
+        <Main />;
+        <NextScript />;
       </body>
+      ;
     </Html>
   )
 }
