c170bc2ef3a856f7d96de2ecc9e79605d742e9eb
yonghyean <kyhyh0334@gmail.com>
2023-04-05 23:20:15 +0900

    chore: hardhat setting

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,7 +2,14 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      from: process.env.DEFAULT_ADDRESS,
+      url: "https://rpc-mumbai.maticvigil.com",
+    },
+  },
   paths: {
     sources: "./src",
   },
--- contracts/src/LiFT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.6;
-
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-contract LiFT is ERC721URIStorage {
-    constructor() {}
-}
