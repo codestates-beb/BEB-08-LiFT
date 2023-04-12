199fa3d7c874096fbebd48fd9d2f1583ecfe1999
gubeomlee <29tigerhg@gmail.com>
2023-04-12 10:20:24 +0900

    approve는 별도 실행해야함. 다수 contract와의 소통을 위해서 interface 컨트랙트 주소 입력 방식 변경

--- market.sol
+++ market.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 interface erc721Interface {
     function ownerOf(uint256 tokenId) external view returns (address); 
-    function approve(address to, uint256 tokenId) external;
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
 }
 
@@ -20,44 +19,35 @@ contract MarketPlace is Ownable {
 
     using SafeMath for uint256;
 
-    uint256 tradingFee = 0; 
+    uint256 tradingFee = 0;
+    uint256 contractId = 0;   
 
     mapping(uint256 => uint256) public sale;
 
-    erc721Interface erc721; //Interface
-
-    //mint.sol 배포 후 market.sol배포 
-    //mint.sol MyNFT의 컨트랙트 주소를 입력하면 두 컨트랙트가 연결된다.
-    function setInterface(address _contractAddress) public onlyOwner returns(bool) {
-        require(_contractAddress != address(0x0));
-        erc721 = erc721Interface(_contractAddress);
-        return true;  
-    } 
-
     // 판매 등록 함수 
-    function saleNFT(uint256 _tokenId, uint256 _price) public {
+    function saleNFT(uint256 _tokenId, uint256 _price, address _contractAddress) public {
         require(_price > 0, "Price must be greater than zero");
-        require(msg.sender == erc721.ownerOf(_tokenId), "Caller is not  token owner!");
+        require(msg.sender == erc721Interface(_contractAddress).ownerOf(_tokenId), "Caller is not  token owner!");
         require(sale[_tokenId] == 0, "This token is already on sale");
         sale[_tokenId] = _price;
         //erc721.approve(address(this), _tokenId); //컨트랙트에게 apporve 권한 부여하는 것은 mint contract에서 실행한다.
         emit SaleNFT(msg.sender, _tokenId, _price);    
     }
 
-    function buyNFT(uint256 _tokenId) public payable {
+    function buyNFT(uint256 _tokenId, address _contractAddress) public payable {
         require(sale[_tokenId] != 0, "NFT is not for sale");
         uint256 price = sale[_tokenId]; //msg.value는 wei 단위여서 eth단위로 환산하기 위한 계산
         require(msg.value.div(1 ether) == price, "Incorrect payment amount");
-        address seller = erc721.ownerOf(_tokenId); 
-        erc721.safeTransferFrom(seller, msg.sender, _tokenId); //컨트랙트 approve권한으로 safeTransferFrom함수 호출, safeTransferFrom함수 호출 시 approve 초기화 
+        address seller = erc721Interface(_contractAddress).ownerOf(_tokenId); 
+        erc721Interface(_contractAddress).safeTransferFrom(seller, msg.sender, _tokenId); //컨트랙트 approve권한으로 safeTransferFrom함수 호출, safeTransferFrom함수 호출 시 approve 초기화 
         payable(seller).transfer(msg.value * (100 - tradingFee) / 100); //컨트랙트에 들어온 이더를 매도자에게 즉시 전달
         delete sale[_tokenId];
         emit BuyNFT(seller, msg.sender, _tokenId, price);          
     }
 
     // removeSale 함수의 기능중 approve 기능은 mint contract의 approve함수를 통해 실행한다. 
-    function removeSale(uint256 _tokenId) public {
-        require(msg.sender == erc721.ownerOf(_tokenId), "Only seller can remove list");
+    function removeSale(uint256 _tokenId, address _contractAddress) public {
+        require(msg.sender == erc721Interface(_contractAddress).ownerOf(_tokenId), "Only seller can remove list");
         delete sale[_tokenId]; 
         //erc721.approve(address(0), _tokenId); // 판매 리스트 제거 시 approve권한 초기화
         emit RemoveSale(msg.sender, _tokenId);
